e82042e28e81f26d11f898cfd0978757586e3abf
Prashant Madhav <prashant.madhav@gmail.com>
2026-01-04 06:53:06 -0700

    ToDoList_2

--- 01_ToDoList/Program.cs
+++ 01_ToDoList/Program.cs
@@ -1,2 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/// <summary> Print options for a simple TODO list application </summary>
+Console.WriteLine("Hello!");
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("[S]ee all TODO's");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+/// <summary> Wait for user input before closing the console window </summary>
+string userInput = Console.ReadLine();
+Console.WriteLine("User Input is: " + userInput);
+
+/// <summary> Example of string concatenation </summary>
+var result = "abc"+ "def"+ "ghi";
+
 Console.ReadKey();
--- 01_ToDoList/obj/Debug/net8.0/01_ToDoList.AssemblyInfo.cs
+++ 01_ToDoList/obj/Debug/net8.0/01_ToDoList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01_ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5187403c50c43f1c48469ce3d24ad59f1bddb6ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad8a6475cc2c9441c2ecde18341c6d186749e269")]
 [assembly: System.Reflection.AssemblyProductAttribute("01_ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01_ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
