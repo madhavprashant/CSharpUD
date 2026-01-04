870c1b7ffac78f8a98939733223f048e022616a9
Prashant Madhav <prashant.madhav@gmail.com>
2026-01-04 08:01:05 -0700

    fundamentals_01

---
+++ 01_Fundamentals/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Hello, learning some operators!");
+var number = 10;
+var isLargerThan5 = number > 5;
+var islargerThanOrEqualTo5 = number >= 5;
+
+/// <summary> checking Modulo/Remiander operator  </summary>
+var isEven = number % 2 == 0; // true if number is even
+var isModulo3Equals1 = number % 3 == 1; // true if remainder of number divided by 3 equals 1
+
+Console.ReadKey();
